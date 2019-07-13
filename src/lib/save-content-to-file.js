f7afa92fe09f03bbc298c3ac873c39aea618010b
silvuss <38167818+silvuss@users.noreply.github.com>
2019-07-13 10:24:34 -0400

    Implement saving content to a file
    Add old-time code (maybe will work)

--- src/lib/save-content-to-file.js
+++ src/lib/save-content-to-file.js
@@ -3,5 +3,22 @@ export default function saveContentToFile(filename, content) {
     // The parameter "content" is a JavaScript object.
     // The function does not return anything.
 
-    // TODO: saving content to file
+    if (!fs.existsSync("./termbase")) {
+        fs.mkdirSync("./termbase");
+    }
+
+    if (!fs.existsSync("./termbase/data")) {
+        fs.mkdirSync("./termbase/data");
+    }
+
+    if (!fs.existsSync(`./termbase/data/${filename}`)) {
+        fs.writeFile(`./termbase/data/${filename}`, content);
+    } else {
+        const confirmed = confirm(`The file 'termbase/data/${filename}' already exists. Do you want to override it?`);
+        if (confirmed) {
+            fs.writeFile(`./termbase/data/${filename}`, content, () => { alert("Data saved."); });
+        } else {
+            alert("Nothing done.");
+        }
+    }
 }
